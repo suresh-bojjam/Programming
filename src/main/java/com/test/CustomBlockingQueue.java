d8cc0d02a5c4985f640d82131e0a2ede86f9051f
BOJJAM, SURESH <SURESH.BOJJAM@Teradata.com>
2018-02-21 14:15:27 +0530

    Blocking Queue atomic count decerement added

--- src/main/java/com/test/CustomBlockingQueue.java
+++ src/main/java/com/test/CustomBlockingQueue.java
@@ -5,32 +5,32 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 interface BlockingQueue<E>{
-	 /**
-     * Inserts the specified element into this queue
-     * only if space is available else
-     * waits for space to become available.
-     */
+	/**
+	 * Inserts the specified element into this queue
+	 * only if space is available else
+	 * waits for space to become available.
+	 */
 	void put(E item)  throws InterruptedException ;
 
 
-    /**
-     * Retrieves and removes the head of this queue
-     * only if elements are available else
-     * waits for element to become available.
-     */
-    E take()  throws InterruptedException;
+	/**
+	 * Retrieves and removes the head of this queue
+	 * only if elements are available else
+	 * waits for element to become available.
+	 */
+	E take()  throws InterruptedException;
 }
 
 public class CustomBlockingQueue<E> implements BlockingQueue<E>{
-	
+
 	private AtomicInteger count;
-	
+
 	private List<E> list;
-	
+
 	private int maxSize;
-	
+
 	public static void main(String[] args) {
-		
+
 	}
 
 	CustomBlockingQueue(int maxSize){
@@ -38,7 +38,7 @@ public class CustomBlockingQueue<E> implements BlockingQueue<E>{
 		this.list=new ArrayList<E>(maxSize);
 		count=new AtomicInteger(0);
 	}
-	
+
 	@Override
 	public synchronized void put(E item) throws InterruptedException {
 		if(list.size()==maxSize) {
@@ -57,6 +57,8 @@ public class CustomBlockingQueue<E> implements BlockingQueue<E>{
 			this.wait();
 		}
 		this.notifyAll();
+		if(!(count.get()==0))
+			count.getAndDecrement();			
 		return list.remove(0);
 	}
 }
